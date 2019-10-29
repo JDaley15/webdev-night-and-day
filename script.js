fada7362ca11d68cec9a85d99b590658a96175e7
Daley James <20095@columbushs.com>
2019-10-29 09:40:45 -0400

    progress

---
+++ script.js
@@ -0,0 +1,13 @@
+$('#orb').click( function() {        
+    if ($(this).hasClass('sun')) {
+        $(this).removeClass('sun').addClass('moon');
+    }
+    else {
+        $(this).removeClass('moon').addClass('sun');
+    }
+    if ($('#sky').hasClass('day')) {
+        $('#sky').removeClass('day').addClass('night');
+    }
+    else {
+        $('#sky').removeClass('night').addClass('day');
+    }
